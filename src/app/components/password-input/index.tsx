246c87b5641bc796a8644f2f18d96450a78b64d0
Giorgi Gvimradze <natceo@gmail.com>
2024-08-02 18:13:43 +0400

    feat: login page markup
    setup of styling as theme as well as login markup

---
+++ src/app/components/password-input/index.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useState } from "react";
+export const PasswordInput = () => {
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  return (
+    <>
+      <div className="space-y-2">
+        <Label htmlFor="password">პაროლი</Label>
+        <Input
+          id="password"
+          type={!showPassword ? "password" : "text"}
+          placeholder="პაროლი"
+        />
+      </div>
+      <div className="flex items-center space-x-2">
+        <Checkbox
+          id="show-password"
+          onCheckedChange={(state: boolean) => {
+            setShowPassword(state);
+          }}
+        />
+        <Label htmlFor="show-password">მაჩვენე პაროლი</Label>
+      </div>
+    </>
+  );
+};
