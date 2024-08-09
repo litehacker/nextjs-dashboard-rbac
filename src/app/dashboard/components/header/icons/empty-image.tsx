caba4b9188128be96bd02e7a09c2bc2629ad341e
Giorgi Gvimradze <natceo@gmail.com>
2024-08-09 16:16:25 +0400

    refactor: improving avatar design

---
+++ src/app/dashboard/components/header/icons/empty-image.tsx
@@ -0,0 +1,59 @@
+export const EmptyAvatar = () => {
+  return (
+    <svg
+      width="52"
+      height="52"
+      viewBox="0 0 52 52"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clip-path="url(#clip0_1001_8012)">
+        <rect x="2" y="2" width="48" height="48" rx="24" fill="#F5F8FF" />
+        <path
+          d="M-5 62C-5 58.8845 -4.19816 55.7996 -2.64027 52.9213C-1.08237 50.043 1.20107 47.4277 4.07969 45.2247C6.95831 43.0218 10.3757 41.2743 14.1368 40.0821C17.8979 38.8898 21.929 38.2762 26 38.2762C30.071 38.2762 34.1021 38.8898 37.8632 40.0821C41.6243 41.2743 45.0417 43.0218 47.9203 45.2247C50.7989 47.4277 53.0824 50.043 54.6403 52.9213C56.1982 55.7996 57 58.8845 57 62L-5 62Z"
+          fill="url(#paint0_linear_1001_8012)"
+        />
+        <path
+          d="M39.7213 22.6286C39.7213 30.1554 33.5781 36.2571 26 36.2571C18.4219 36.2571 12.2787 30.1554 12.2787 22.6286C12.2787 15.1017 18.4219 9 26 9C33.5781 9 39.7213 15.1017 39.7213 22.6286Z"
+          fill="url(#paint1_linear_1001_8012)"
+        />
+      </g>
+      <rect
+        x="1.25"
+        y="1.25"
+        width="49.5"
+        height="49.5"
+        rx="24.75"
+        stroke="white"
+        strokeWidth="1.5"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_1001_8012"
+          x1="26"
+          y1="9"
+          x2="26"
+          y2="62"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#2670FF" />
+          <stop offset="1" stop-color="#F5F8FF" />
+        </linearGradient>
+        <linearGradient
+          id="paint1_linear_1001_8012"
+          x1="26"
+          y1="9"
+          x2="26"
+          y2="62"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#2670FF" />
+          <stop offset="1" stop-color="#F5F8FF" />
+        </linearGradient>
+        <clipPath id="clip0_1001_8012">
+          <rect x="2" y="2" width="48" height="48" rx="24" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
--- src/app/dashboard/components/header/index.tsx
+++ src/app/dashboard/components/header/index.tsx
@@ -1,17 +1,33 @@
 "use server";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
+import { Pencil, Trash } from "lucide-react";
 import { ReactNode } from "react";
+import { EmptyAvatar } from "./icons/empty-image";
 
 export async function DashboardHeader({ children }: { children: ReactNode }) {
   return (
     <header className="flex items-center justify-between pb-6 pt-[10px]">
       <h1 className="text-2xl font-semibold pl-3">{children}</h1>
       <div className="flex items-center space-x-4">
-        <Avatar>
-          <AvatarImage src="/placeholder-user.jpg" alt="User" />
-          <AvatarFallback>U</AvatarFallback>
-        </Avatar>
+        <div className="w-6 h-6 relative">
+          <Avatar className="w-full h-full">
+            <AvatarImage src="/uploads/image.png" alt="User" />
+            <AvatarFallback>
+              <EmptyAvatar />
+            </AvatarFallback>
+          </Avatar>
+          <div className="absolute left-1/2 transform -translate-x-1/2 px-1 top-3/4 bg-white shadow-sm flex gap-0 rounded-full items-center">
+            <button>
+              <Pencil className="p-1.5" />
+              <span className="sr-only">Edit</span>
+            </button>
+            <button>
+              <Trash className="p-1.5" />
+              <span className="sr-only">Upload</span>
+            </button>
+          </div>
+        </div>
         <Button variant="outline" className="text-gray-600">
           არჩეული ჩანართები
         </Button>
